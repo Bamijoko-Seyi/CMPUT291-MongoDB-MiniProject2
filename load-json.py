900cd87b94ee279d9b7e6224f300d43eb2f57dd5
bamijoko-UA <122421393+bamijoko-UA@users.noreply.github.com>
2023-11-28 16:23:23 -0700

    made some changes

--- load-json.py
+++ load-json.py
@@ -7,7 +7,7 @@ import time
 
 def main():
     
-    ##Gets the location of the json and the port number through the terminal
+    # Gets the location of the json and the port number through the terminal
     parser = argparse.ArgumentParser(description="Load json script")
     parser.add_argument("--json", help="JSON file to be read for the program", required=True)
     parser.add_argument("--port", help="Port for the program", required=True)
@@ -15,14 +15,14 @@ def main():
 
     start_time = time.time()
 
-    ##Connects the database using the port number provided
+    # Connects the database using the port number provided
     client = MongoClient('mongodb://localhost:'+ args.port)
     db = client["291db"]
 
     tweets = db["tweets"]
     tweets.drop()
 
-    ##Inserts the data in batches in to the tweets collection
+    # Inserts the data in batches in to the tweets collection
     os.system('mongoimport --db=291db --collection=tweets --port={} --file={} --batchSize=10000 --numInsertionWorkers=20'.format(args.port,args.json))
 
     end_time = time.time()
