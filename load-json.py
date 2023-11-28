d249d784e94e256240766865e4a978f33e498df2
bamijoko-UA <122421393+bamijoko-UA@users.noreply.github.com>
2023-11-28 12:37:13 -0700

    Added build time

--- load-json.py
+++ load-json.py
@@ -3,12 +3,15 @@ import pymongo
 from pymongo import MongoClient
 from pymongo import TEXT
 import os
+import time
 
 def main():
     parser = argparse.ArgumentParser(description="Load json script")
     parser.add_argument("--json", help="JSON file to be read for the program", required=True)
     parser.add_argument("--port", help="Port for the program", required=True)
     args = parser.parse_args()
+
+    start_time = time.time()
 
     client = MongoClient('mongodb://localhost:'+ args.port)
     db = client["291db"]
@@ -18,8 +21,10 @@ def main():
 
     os.system('mongoimport --db=291db --collection=tweets --port={} --file={} --batchSize=10000 --numInsertionWorkers=20'.format(args.port,args.json))
 
-    print("Json file successfully imported!")        
+    end_time = time.time()
 
+    print("Json file successfully imported!")
+    print(f"Time taken: {end_time - start_time} seconds")
 
 if __name__ == "__main__":
     main()
