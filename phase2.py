349b106ff85dec585a03c7ab140fd9b0a28fbc30
Rachit Anugula <127341193+ranugula@users.noreply.github.com>
2023-11-27 18:19:30 -0700

    Shortened top_users()

--- phase2.py
+++ phase2.py
@@ -162,61 +162,46 @@ def list_top_tweets(tweets):
             print("Invalid input. Please enter a number.")
 
 def list_top_users(tweets):
-    n = input("Enter the number of users you would like to list: ")
-    print()
-    while not n.isdigit():
-        n = input("Invalid. Please enter a valid number: ")
-        print()
-
-    results = list(tweets.find().sort("user.followersCount", pymongo.DESCENDING).limit(int(n)))
-    user_index_to_id_map = {}
-
     while True:
-        for index, result in enumerate(results, start = 1):
-            user = result.get("user", {})
-            user_id, username, display_name = user.get('id', 'N/A'), user.get('username', 'N/A'), user.get('displayname', 'N/A')
-            followers_count = user.get('followersCount', 'N/A')
-            user_index_to_id_map[index] = user_id
-            print(f"{index}. User ID: {user_id} | Username: {username:20} | Display name: {display_name:20} | Followers Count: {followers_count}")
+        n = input("Enter the number of users you would like to list, or type 'menu' to return: ")
         print()
+        if n.lower() == 'menu':
+            return
+        if not n.isdigit():
+            print("Invalid. Please enter a valid number.\n")
+            continue
 
-        user_input = input("Options:\n1 - Select user\n2 - Go back\n\nInput: ")
-        print()
-        while user_input not in ["1", "2"]:
-            user_input = input("Invalid. Please select from one of the following options:\n1 - Select user\n2 - Go back\n\nInput: ")
+        results = list(tweets.aggregate([
+                {"$group": {"_id": "$user.id", "username": {"$first": "$user.username"}, "displayname": {"$first": "$user.displayname"}, "followersCount": {"$first": "$user.followersCount"}}},
+                {"$sort": {"followersCount": -1}},
+                {"$limit": int(n)}
+            ]))
+
+        if not results:
+            print("No users found.")
+            continue
+        else:
+            no_duplicates = []
+            for index, user in enumerate(results, start=1):
+                if user not in no_duplicates:
+                    no_duplicates.append(user)
+                    print(f"{index:4}. Username: {user['username']:20} | Display name: {user['displayname']:45} | Followers Count: {user['followersCount']}")
             print()
 
-        if user_input == "1":
-            while True:
-                user_input = input("Select a user by number to see more information: ")
-                print()
-                while not user_input.isdigit():
-                    user_input = input("Invalid selection. Please select one of the numbers displayed above: ")
-                    print()
-                
-                selected_user_id = user_index_to_id_map[int(user_input) - 1]
-                
-                user_id, user_info = '', ''
-                for result in results:
-                    user = result.get("user", {})
-                    user_id = user.get("id", "N/A")
-                    if user_id == selected_user_id:
-                        user_info = result
-                        break
+        user_selection = input("Enter the number of the user to see full details, or type 'menu' to return: ")
+        print()
+        if user_selection.lower() == 'menu':
+            return
 
-                if user_info:
-                    pprint.pprint(user_info)
-                    print()
-                else:
-                    print("User information not found.\n")
-                
-                user_input = input("Options:\n1 - Go back\n\nInput: ")
-                while user_input != "1":
-                    user_input = input("Invalid. Please select one of the following options:\n1 - Go back\n\nInput: ")
-                break
-
-        else:
-            break
+        try:
+            user_number = int(user_selection)
+            if 0 < user_number <= len(results):
+                selected_user = results[user_number - 1]
+                pprint.pprint(selected_user)
+            else:
+                print("Invalid user number entered.\n")
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
             
 def compose_tweet(tweets):
     content = input("Enter the tweet content: ")
